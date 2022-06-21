887f1c311b9aa6634fe845f9f3be294755681771
Sally Diament <sally.barzilai@gmail.com>
2022-06-21 14:58:22 -0300

    incluindo o 2o requisito

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        teste
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
