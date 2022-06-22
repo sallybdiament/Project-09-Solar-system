bc0129735cc1d7f355c44d52fa1d126f7f7a58b9
Sally Diament <sally.barzilai@gmail.com>
2022-06-22 10:01:54 -0300

    Finalizando o projeto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-        <h1 className='Header'>Sistema Solar</h1>
+        <h1 className="Header">Sistema Solar</h1>
       </header>
     );
   }
